61944cc00020efb3d02e0af32c2325ade9c4dbb5
Rania Yqb <raniayaqoubi1995@gmail.com>
2022-08-06 11:28:52 +0200

    Webpack Encore

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
