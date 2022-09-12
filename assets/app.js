632aeb21213747df2372b034489b2f40f063cf9a
Rania Yqb <raniayaqoubi1995@gmail.com>
2022-09-12 16:13:23 +0200

    flash messages

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,26 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
+
+
+const flashElSuccess = document.querySelectorAll('.flash-el-success')
+const flashElDanger = document.querySelectorAll('.flash-el-danger')
+
+
+//flashMessageDanger.addEventListener('click', remove)
+//flashMessage.addEventListener('click', remove)
+
+flashElSuccess.forEach(flashElement =>{
+        flashElement.addEventListener('click', ()=>{
+            const flashMessage = document.querySelector('.flash-messages')
+            flashMessage.style.display = "none"
+        })
+})
+
+flashElDanger.forEach(flashElement =>{
+    flashElement.addEventListener('click', ()=>{
+        const flashMessageDanger = document.querySelector('.flash-danger-messages')
+        flashMessageDanger.style.display = "none"
+    })
+})
+
